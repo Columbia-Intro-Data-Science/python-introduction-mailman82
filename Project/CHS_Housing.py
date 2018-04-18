1a37f3c6160b46973436b5e68e092639fd8357a8
mailman82 <ljc2162@columbia.edu>
2018-04-17 22:08:21 -0400

    Update CHS_Housing.py

--- Project/CHS_Housing.py
+++ Project/CHS_Housing.py
@@ -1,38 +1,12 @@
-
-# coding: utf-8
-
-# In[2]:
-
-# read data into a DataFrame
-import pandas as pd
-import pylab as plt
-import seaborn
-from sklearn.linear_model import LinearRegression
-import numpy.random as nprnd
-import random
-import json
-import numpy as np
-pd.set_option('display.max_columns', 500)
-get_ipython().magic(u'matplotlib inline')
-
 import webapp2
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, Liam!')
+        self.response.write('Hello, Liam - This works, now you get to work!')
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
-
-df = pd.read_csv('https://github.com/Columbia-Intro-Data-Science/python-introduction-mailman82/blob/master/Project/CHSHousingProjectData.csv')
-df.head()
-
-
-# In[ ]:
-
-
-
