bbed9d01bd126feb4e44d42e18d90ce87554e1cf
mailman82 <ljc2162@columbia.edu>
2018-04-17 22:03:24 -0400

    Update CHS_Housing.py

--- Project/CHS_Housing.py
+++ Project/CHS_Housing.py
@@ -15,6 +15,19 @@ import numpy as np
 pd.set_option('display.max_columns', 500)
 get_ipython().magic(u'matplotlib inline')
 
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, Liam!')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
+
 df = pd.read_csv('https://github.com/Columbia-Intro-Data-Science/python-introduction-mailman82/blob/master/Project/CHSHousingProjectData.csv')
 df.head()
 
